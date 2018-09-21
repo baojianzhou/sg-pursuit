9118f5e9f0802d1c6297ca1c6b7e986e53ec63d9
Baojian Zhou <bzhou6@albany.edu>
2018-09-21 10:41:06 -0400

    dd

--- utils/data_convert.py
+++ utils/data_convert.py
@@ -5,10 +5,7 @@ import sys
 import time
 import cPickle
 import numpy as np
-import multiprocessing
 from itertools import product
-from sparse_learning.proj_algo import head_proj
-from sparse_learning.proj_algo import tail_proj
 
 os.environ["MKL_NUM_THREADS"] = "1"
 os.environ["NUMEXPR_NUM_THREADS"] = "1"
@@ -17,7 +14,7 @@ os.environ["OMP_NUM_THREADS"] = "1"
 root_path = '../input/data_chicago/'
 
 
-def data_convert():
+def data_convert_chicago_crime():
     for test_case, event_type in product(range(52), ['BATTERY', 'BURGLARY']):
         data = {'n': 0,
                 'p': 0,
@@ -61,12 +58,78 @@ def data_convert():
                     data['true_sub_feature'] = sub_feature
                     data['true_y'] = np.zeros(p)
                     data['true_y'][sub_feature] = 1.
-        data['data_matrix'] = np.asarray(data['data_matrix'],
-                                         dtype=np.float64)
+        data['data_matrix'] = np.asarray(data['data_matrix'], dtype=np.float64)
         data['edges'] = np.asarray(data['edges'], dtype=int)
-        data['costs'] = np.asarray(data['costs'],
-                                   dtype=np.float64)
+        data['costs'] = np.asarray(data['costs'], dtype=np.float64)
         file_name = root_path + 'chicago_%s_case_%d.pkl' % \
                     (event_type, test_case)
         bz2_f = bz2.BZ2File(file_name, 'wb')
         cPickle.dump(data, bz2_f)
+
+
+def data_convert_yelp():
+    root_path = '/home/baojian/Yelp/'
+    for test_case, event_type in product([21], ['2014_2015']):
+        data = {'test_case': test_case,
+                'n': 0,
+                'p': 0,
+                'k': 0,
+                's': 0,
+                'm': 0,
+                'data_matrix': [],
+                'nodes_dict': dict(),
+                'words_dict': dict(),
+                'edges': [],
+                'costs': [],
+                'event_type': event_type,
+                'true_sub_graph': [],  # nodes
+                'true_sub_feature': [],  # words
+                'true_x': None,
+                'true_y': None}
+        print(event_type, test_case)
+        with open(root_path + 'graph/processed_%s_test_case_%d.txt'
+                  % (event_type, test_case)) as f:
+            n, p, k, s, m = 0, 0, 0, 0, 0
+            for ind, each_line in enumerate(f.readlines()):
+                each_line = each_line.lstrip().rstrip()
+                if ind == 0:
+                    data['n'] = int(each_line.split(' ')[0])
+                    data['p'] = int(each_line.split(' ')[1])
+                    data['k'] = int(each_line.split(' ')[2])
+                    data['s'] = int(each_line.split(' ')[3])
+                    data['m'] = int(each_line.split(' ')[4])
+                    n, p, m = data['n'], data['p'], data['m']
+                elif 1 <= ind <= n:
+                    arr = [float(_) for _ in each_line.split(' ')]
+                    data['data_matrix'].append(arr)
+                elif n + 1 <= ind <= 2 * n:
+                    key_ = each_line.split(' ')[0]
+                    data['nodes_dict'][key_] = int(each_line.split(' ')[1])
+                elif 2 * n + 1 <= ind <= 2 * n + p:
+                    key_ = each_line.split(' ')[0]
+                    data['words_dict'][key_] = int(each_line.split(' ')[1])
+                elif 2 * n + p + 1 == ind:
+                    sub_graph = [int(_) for _ in each_line.split(' ')]
+                    data['true_sub_graph'] = sub_graph
+                    data['true_x'] = np.zeros(n)
+                    data['true_x'][sub_graph] = 1.
+                elif 2 * n + p + 2 == ind:
+                    sub_feature = [int(_) for _ in each_line.split(' ')]
+                    data['true_sub_feature'] = sub_feature
+                    data['true_y'] = np.zeros(p)
+                    data['true_y'][sub_feature] = 1.
+                elif 2 * n + p + 3 <= ind <= 2 * n + p + 3 + m:
+                    edge = [each_line.split(' ')[0],
+                            each_line.split(' ')[1]]
+                    data['edges'].append(edge)
+                    data['costs'].append(1.0)  # use default value.
+        data['data_matrix'] = np.asarray(data['data_matrix'], dtype=np.float64)
+        data['edges'] = np.asarray(data['edges'], dtype=int)
+        data['costs'] = np.asarray(data['costs'], dtype=np.float64)
+        file_name = root_path + 'chicago_%s_case_%d.pkl' % \
+                    (event_type, test_case)
+        bz2_f = bz2.BZ2File(file_name, 'wb')
+        cPickle.dump(data, bz2_f)
+
+
+data_convert_yelp()
