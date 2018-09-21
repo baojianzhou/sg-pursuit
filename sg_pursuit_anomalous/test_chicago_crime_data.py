d1c7765d7f1d1af9eb3c6ccff00b0ad62aa90431
Baojian Zhou <bzhou6@albany.edu>
2018-09-20 21:17:32 -0400

    dd

--- sg_pursuit_anomalous/test_chicago_crime_data.py
+++ sg_pursuit_anomalous/test_chicago_crime_data.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-# !/usr/bin/python
-import cPickle
 import bz2
+import cPickle
 import numpy as np
+from itertools import product
 from sparse_learning.proj_algo import head_proj
 from sparse_learning.proj_algo import tail_proj
 
+root_path = '/network/rit/lab/ceashpc/bz383376/data/icdm17/CrimesOfChicago/'
 
+
+# elevated mean scan statistic
 class FuncEMS(object):
     def __init__(self, data_matrix, lambda_):
         self.data_matrix = data_matrix
@@ -116,50 +119,58 @@ class FuncEMS(object):
 
 
 def data_convert():
-    root_path = '/network/rit/lab/ceashpc/' \
-                'bz383376/data/icdm17/CrimesOfChicago/'
-    for test_case in range(52):
-        for event_type in ['BATTERY', 'BURGLARY']:
-            data = {'test_case': test_case, 'data_matrix': [], 'edges': [],
-                    'costs': [], 'event_type': event_type,
-                    'true_sub_graph': [], 'true_sub_feature': []}
-            print(event_type, test_case)
-            with open(root_path + 'graph/processed_%s_test_case_%d.txt'
-                      % (event_type, test_case)) as f:
-                n, m, p = 0, 0, 0
-                for ind, each_line in enumerate(f.readlines()):
-                    each_line = each_line.lstrip().rstrip()
-                    if ind == 0:
-                        data['n'] = int(each_line.split(' ')[0])
-                        data['p'] = int(each_line.split(' ')[1])
-                        n, p = data['n'], data['p']
-                    elif 1 <= ind <= n:
-                        arr = [float(_) for _ in each_line.split(' ')]
-                        data['data_matrix'].append(arr)
-                    elif ind == n + 1:
-                        data['m'] = int(each_line)
-                        m = int(each_line)
-                    elif n + 2 <= ind <= m + n + 1:
-                        edge = [each_line.split(' ')[0],
-                                each_line.split(' ')[1]]
-                        data['edges'].append(edge)
-                        data['costs'].append(
-                            float(each_line.split(' ')[2]))
-                    elif ind == m + n + 2:
-                        sub_graph = [int(_) for _ in each_line.split(' ')]
-                        data['true_sub_graph'] = sub_graph
-                    elif ind == m + n + 3:
-                        sub_feature = [int(_) for _ in each_line.split(' ')]
-                        data['true_sub_feature'] = sub_feature
-            data['data_matrix'] = np.asarray(data['data_matrix'],
-                                             dtype=np.float64)
-            data['edges'] = np.asarray(data['edges'], dtype=int)
-            data['costs'] = np.asarray(data['costs'],
-                                       dtype=np.float64)
-            file_name = root_path + 'chicago_%s_case_%d.pkl' % \
-                        (event_type, test_case)
-            bz2_f = bz2.BZ2File(file_name, 'wb')
-            cPickle.dump(data, bz2_f)
+    for test_case, event_type in product(range(52), ['BATTERY', 'BURGLARY']):
+        data = {'n': 0,
+                'p': 0,
+                'test_case': test_case,
+                'data_matrix': [],
+                'edges': [],
+                'costs': [],
+                'event_type': event_type,
+                'true_sub_graph': [],
+                'true_sub_feature': [],
+                'true_x': None,
+                'true_y': None}
+        print(event_type, test_case)
+        with open(root_path + 'graph/processed_%s_test_case_%d.txt'
+                  % (event_type, test_case)) as f:
+            n, m, p = 0, 0, 0
+            for ind, each_line in enumerate(f.readlines()):
+                each_line = each_line.lstrip().rstrip()
+                if ind == 0:
+                    data['n'] = int(each_line.split(' ')[0])
+                    data['p'] = int(each_line.split(' ')[1])
+                    n, p = data['n'], data['p']
+                elif 1 <= ind <= n:
+                    arr = [float(_) for _ in each_line.split(' ')]
+                    data['data_matrix'].append(arr)
+                elif ind == n + 1:
+                    data['m'] = int(each_line)
+                    m = int(each_line)
+                elif n + 2 <= ind <= m + n + 1:
+                    edge = [each_line.split(' ')[0],
+                            each_line.split(' ')[1]]
+                    data['edges'].append(edge)
+                    data['costs'].append(float(each_line.split(' ')[2]))
+                elif ind == m + n + 2:
+                    sub_graph = [int(_) for _ in each_line.split(' ')]
+                    data['true_sub_graph'] = sub_graph
+                    data['true_x'] = np.zeros(n)
+                    data['true_x'][sub_graph] = 1.
+                elif ind == m + n + 3:
+                    sub_feature = [int(_) for _ in each_line.split(' ')]
+                    data['true_sub_feature'] = sub_feature
+                    data['true_y'] = np.zeros(p)
+                    data['true_y'][sub_feature] = 1.
+        data['data_matrix'] = np.asarray(data['data_matrix'],
+                                         dtype=np.float64)
+        data['edges'] = np.asarray(data['edges'], dtype=int)
+        data['costs'] = np.asarray(data['costs'],
+                                   dtype=np.float64)
+        file_name = root_path + 'chicago_%s_case_%d.pkl' % \
+                    (event_type, test_case)
+        bz2_f = bz2.BZ2File(file_name, 'wb')
+        cPickle.dump(data, bz2_f)
 
 
 def normalize_gradient(x, grad):
@@ -219,8 +230,6 @@ def sg_pursuit_algo(para):
 
 
 def main():
-    root_path = '/network/rit/lab/ceashpc/' \
-                'bz383376/data/icdm17/CrimesOfChicago/'
     for test_case in range(52):
         for event_type in ['BATTERY', 'BURGLARY']:
             file_name = root_path + 'chicago_%s_case_%d.pkl' % \
